742a2fa3c25242a66d5380f6dfe0598e51f76e21
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2026-02-09 14:51:02 -0500

    Updated Playwright GitHub Actions configuration.

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'docker-compose up',
+    url: 'http://localhost:3000',
+    timeout: 180 * 1000, // wait up to 3 minutes for Docker to build
+    reuseExistingServer: !process.env.CI,
+  },
 });
