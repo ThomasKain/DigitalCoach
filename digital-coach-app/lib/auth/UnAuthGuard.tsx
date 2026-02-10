7b5518f86d11b7794a4c11e1c598105598cbe5c0
Britt Li Kendle <90359619+Bkendle1@users.noreply.github.com>
2026-02-09 21:32:03 -0500

    Modified UnAuthGuard to redirect users to /auth/register if the user needs to complete their registration, e.g. select profile picture.

--- digital-coach-app/lib/auth/UnAuthGuard.tsx
+++ digital-coach-app/lib/auth/UnAuthGuard.tsx
@@ -2,12 +2,46 @@ import { WithRouterProps } from "next/dist/client/with-router";
 import { withRouter } from "next/router";
 import { PropsWithChildren, useEffect } from "react";
 import AuthService from "./AuthService";
+import UserService from "../user/UserService";
 
 function UnAuthGuard({ children, router }: PropsWithChildren<WithRouterProps>) {
+
   useEffect(() => {
-    AuthService.onAuthStateChanged((user) => {
-      if (!!user) router.push("/");
+    let isMounted = true;
+    const unsubscribe = AuthService.onAuthStateChanged(async (user) => {
+      // if (!!user) router.push("/");
+      if (!!user && isMounted) {
+      
+        try {
+        // get user doc to check registration status
+        const userDoc = await UserService.getUser(user.uid);
+        if (!isMounted) return; // Check after async operation
+        
+        if (userDoc.exists()) {
+          // if the user is authenticated but still needs to finish profile registration, go to register page
+          if (!userDoc.data()?.registrationCompletedAt) {
+            router.push("/auth/register");
+          } 
+          else {
+            // authenticated user has completed profile registration, go home
+            router.push("/");
+          }
+        } else {
+          // if user document doesn't exist, redirect to register page
+          router.push("/auth/register");
+        }
+        
+       } catch (error) {
+          // cant find user doc
+          console.error("Error fetching user document:", error);
+       } 
+      } 
     });
+
+    return () => {
+      isMounted = false;
+      unsubscribe?.();
+    };
   }, [router]);
 
   return <>{children}</>;
