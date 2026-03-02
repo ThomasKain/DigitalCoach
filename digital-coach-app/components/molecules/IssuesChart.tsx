03fbfbb8a19658d2dc6ba794169e8381b1c172eb
Thomas Kain <tkain@stevens.edu>
2026-03-02 18:08:46 -0500

    Fix duplicated axis labels on issue chart

--- digital-coach-app/components/molecules/IssuesChart.tsx
+++ digital-coach-app/components/molecules/IssuesChart.tsx
@@ -37,7 +37,7 @@ const options = {
     responsive: true,
     maintainAspectRatio: false,
     scales:{
-        yAxes: {
+        y: {
             ticks:{
                 mirror:true,
                 z: 1
